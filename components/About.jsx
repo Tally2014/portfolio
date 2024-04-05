857d43380b17f18053df114c3c9ff4aac59f715c
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-04-05 14:19:14 +0200

    style - Add tsx content in the About component

--- components/About.jsx
+++ components/About.jsx
@@ -11,7 +11,37 @@ export default function About() {
       transition={{ delay: 0.175 }}
       id="about"
     >
-      <h1>About Me</h1>
+      <h2 className="text-3xl font-medium capitalize mb-8 text-center">
+        About Me
+      </h2>
+      <p className="mb-3">
+        As a seasoned professional in the{" "}
+        <span className="font-medium">Television industry</span> at{" "}
+        <span className="font-medium">Newzroom Afrika</span>, I embraced my
+        passion for programming by diving into coding bootcamps where I
+        learned👨‍💻 <span className="font-medium">C# and web development</span>.
+        This knowledge empowered me to create applications for{" "}
+        <span className="italic">TV live graphics</span>. I love comming up with
+        new ideas that are <span className="underline">innovative</span>,
+        enhances my work <span className="underline">efficiency</span> and
+        simplifying complex tasks. My core programming stack includes{" "}
+        <span className="font-medium">
+          C#, WPF, .NET MAUI, JavaScript, React, Node.js, and MongoDB
+        </span>
+        . Professionally, I use tools like{" "}
+        <span className="font-medium">
+          Avid Maestro Designer, Avid Page Editor, Avid News Controller, Viz
+          Artist and Adobe Creative Suite
+        </span>
+        . Committed to growth, I embrace my ability to quickly learn new
+        technologies💪.
+      </p>
+
+      <p>
+        <span className="italic">When I'm not coding</span>, I enjoy playing
+        video games, watching Anime, and playing with my nephew. I also enjoy{" "}
+        <span className="font-medium">learning new things</span>🤩.
+      </p>
     </motion.section>
   );
 }
