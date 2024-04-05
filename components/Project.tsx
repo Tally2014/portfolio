fe34e2093b7eae4ffac57ff40e8d4f7ca1edd1bc
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-04-05 19:51:12 +0200

    style - Impliment framer-motion in Project component

--- components/Project.tsx
+++ components/Project.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from 'react';
+import {useRef} from 'react';
 import { motion, useScroll, useTransform } from "framer-motion";
 import { projectsData } from "@/lib/data";
 import Image from "next/image";
@@ -12,20 +12,39 @@ export default function Project({
     tags,
     imageUrl,
   }: ProjectProps) {
+    const ref = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["0 1", "1.33 1"],
+  });
+  const scaleProgess = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
+  const opacityProgess = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
+
   return (
-    <div  className="group mb-3 sm:mb-8 last:mb-0">
-        <section className='bg-gray-100 max-w-[42] border border-black/5 rounded-lg overflow-hidden sm:h-[20rem] relative hover:bg-gray-200 transition '>
-            <div className='pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full'>
+    <motion.div ref={ref} style={{ scale: scaleProgess, opacity: opacityProgess}}  className="group mb-3 sm:mb-8 last:mb-0">
+        <section className='bg-gray-100 max-w-[42] border border-black/5 rounded-lg overflow-hidden sm:h-[20rem] relative hover:bg-gray-200 transition sm:group-even:pl-8'>
+            <div className='pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[24rem]'>
                 <h3 className='text-2xl font-semibold'>{title}</h3>
                 <p className='mt-2 leading-relaxed text-gray-700'>{description}</p>
                 <ul className='flex flex-wrap mt-4 gap-2 sm:mt-auto'>
                     {tags.map((tag, index) => (
-                        <li key={index}>{tag}</li>
+                        <li className='bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider  text-white rounded-full' key={index}>{tag}</li>
                     ))}
                 </ul>
             </div>
-            <Image src={imageUrl} alt={`${title}'s Image`} quality={95} className='absolute hidden sm:block top-8 -right-[100px] w-[28.25rem] rounded-t-lg shadow-2xl'/>
+            <Image src={imageUrl} alt={`${title}'s Image`} quality={95} className='absolute hidden sm:block top-8 -right-[100px] w-[28.25rem] rounded-t-lg shadow-2xl
+            transition 
+            group-hover:scale-[1.04]
+            group-hover:-translate-x-3
+            group-hover:translate-y-3
+            group-hover:-rotate-2
+
+            group-even:group-hover:translate-x-3
+            group-even:group-hover:translate-y-3
+            group-even:group-hover:rotate-2
+            
+            group-even:right-[initial] group-even:-left-[100px]'/>
         </section>
-    </div>
+    </motion.div>
   )
 }
