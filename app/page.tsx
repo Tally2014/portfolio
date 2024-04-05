51a397ae3810d3a6a5f2c2c8d4cf7be6f47557a3
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-04-05 19:14:27 +0200

    add - Create Projects and Project components

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Intro from '@/components/Intro';
 import SectionDivider from '@/components/SectionDivider';
 import About from '@/components/About';
+import Projects from '@/components/Projects';
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
       <Intro/>
       <SectionDivider/>
       <About/>
+      <Projects/>
     </main>
   )
 }
