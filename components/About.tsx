e6aa67168937699d2928fd384ae78121f3ecc4b6
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-04-06 22:05:01 +0200

    logic - Impliment react-intersection-observer to the first 3 navbar links

--- components/About.tsx
+++ components/About.tsx
@@ -1,9 +1,22 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import { motion } from "framer-motion";
 import SectionHeading from "./SectionHeading";
+import { useInView } from "react-intersection-observer";
+import { useActiveSectionContext } from "@/context/ActiveSectionContextProvider";
 
 export default function About() {
+  const { ref, inView } = useInView({
+    threshold: 0.75,
+  });
+  const { setActiveSection } = useActiveSectionContext();
+
+  useEffect(() => {
+    if (inView) {
+      setActiveSection("About");
+    }
+  }, [inView, setActiveSection]);
+
   return (
     <motion.section
       className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
@@ -11,6 +24,7 @@ export default function About() {
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.175 }}
       id="about"
+      ref={ref}
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,20 +1,32 @@
 "use client";
-import React from 'react'
+import React, { useEffect } from "react";
 import SectionHeading from "./SectionHeading";
-import Project from './Project';
+import Project from "./Project";
 import { projectsData } from "@/lib/data";
+import { useInView } from "react-intersection-observer";
+import { useActiveSectionContext } from "@/context/ActiveSectionContextProvider";
 
 export default function Projects() {
+  const { ref, inView } = useInView({
+    threshold: 0.5,
+  });
+  const { setActiveSection } = useActiveSectionContext();
+
+  useEffect(() => {
+    if (inView) {
+      setActiveSection("Projects");
+    }
+  }, [inView, setActiveSection]);
   return (
-    <section id="projects" className="scroll-mt-28 mb-28">
-        <SectionHeading>My projects</SectionHeading>
-        <div>
-            {projectsData.map((project, index) =>(
-              <React.Fragment key={index}>
-                <Project {...project} />
-              </React.Fragment>
-            ))}
-        </div>
+    <section id="projects" className="scroll-mt-28 mb-28" ref={ref}>
+      <SectionHeading>My projects</SectionHeading>
+      <div>
+        {projectsData.map((project, index) => (
+          <React.Fragment key={index}>
+            <Project {...project} />
+          </React.Fragment>
+        ))}
+      </div>
     </section>
-  )
+  );
 }
