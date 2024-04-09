554374162dabf69230f6f09ee89cd39c2046a412
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-04-09 15:14:09 +0200

    style - add Skills component and import in the page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Intro from "@/components/Intro";
 import SectionDivider from "@/components/SectionDivider";
 import About from "@/components/About";
 import Projects from "@/components/Projects";
+import Skills from "@/components/Skills";
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
@@ -9,7 +10,7 @@ export default function Home() {
       <SectionDivider />
       <About />
       <Projects />
-      <SectionDivider />
+      <Skills />
       <SectionDivider />
       <SectionDivider />
       <SectionDivider />
---
+++ components/Skills.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import SectionHeading from "./SectionHeading";
+import { skillsData } from "@/lib/data";
+
+export default function Skills() {
+  return (
+    <section>
+      <SectionHeading>My skills</SectionHeading>
+    </section>
+  );
+}
