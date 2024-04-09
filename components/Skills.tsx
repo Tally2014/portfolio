803a4af96342a4a051ae17f64d640571ae63f9ee
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-04-09 16:12:04 +0200

    style -  impliment framer-motion animation for the skills tags

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -4,6 +4,21 @@ import React, { useEffect } from "react";
 import SectionHeading from "./SectionHeading";
 import { skillsData } from "@/lib/data";
 import { useSectionInView } from "@/lib/hooks";
+import { motion } from "framer-motion";
+
+const fadeInAnimationVariants = {
+  initial: {
+    opacity: 0,
+    y: 100,
+  },
+  animate: (index: number) => ({
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: 0.05 * index,
+    },
+  }),
+};
 
 export default function Skills() {
   const { ref } = useSectionInView("Skills");
@@ -16,12 +31,19 @@ export default function Skills() {
       <SectionHeading>My skills</SectionHeading>
       <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
         {skillsData.map((skill, index) => (
-          <li
+          <motion.li
             key={index}
             className="bg-white border border-gray-500 rounded-xl px-5 py-3"
+            variants={fadeInAnimationVariants}
+            initial="initial"
+            whileInView="animate"
+            viewport={{
+              once: true,
+            }}
+            custom={index}
           >
             {skill}
-          </li>
+          </motion.li>
         ))}
       </ul>
     </section>
