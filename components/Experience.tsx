2ba1c905a8d7496cfae06110e340afed9ac1aff8
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-04-09 23:38:40 +0200

    logic - impliment  useSectionView navigation for Expirience header link selected highlight

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,10 +1,12 @@
+"use client";
 import React from "react";
 import SectionHeading from "./SectionHeading";
 import { useSectionInView } from "@/lib/hooks";
 
 export default function Experience() {
+  const { ref } = useSectionInView("Experience");
   return (
-    <section>
+    <section id="experience" ref={ref}>
       <SectionHeading>My experience</SectionHeading>
     </section>
   );
