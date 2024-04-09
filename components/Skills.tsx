3c8625d4490e9a5ccba540a28f42ab3b05e697ea
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-04-09 15:52:50 +0200

    logic - impliment useSectionView for Skills header  links selected highlight

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,10 +1,18 @@
-import React from "react";
+"use client";
+
+import React, { useEffect } from "react";
 import SectionHeading from "./SectionHeading";
 import { skillsData } from "@/lib/data";
+import { useSectionInView } from "@/lib/hooks";
 
 export default function Skills() {
+  const { ref } = useSectionInView("Skills");
   return (
-    <section>
+    <section
+      id="skills"
+      ref={ref}
+      className="mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40"
+    >
       <SectionHeading>My skills</SectionHeading>
       <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
         {skillsData.map((skill, index) => (
