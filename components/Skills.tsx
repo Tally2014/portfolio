09160b34ea1c61541ef829dc0981871913ba9561
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-04-09 15:31:06 +0200

    style - tailwind css styling for the skills

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -6,9 +6,14 @@ export default function Skills() {
   return (
     <section>
       <SectionHeading>My skills</SectionHeading>
-      <ul>
+      <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
         {skillsData.map((skill, index) => (
-          <li key={index}>{skill}</li>
+          <li
+            key={index}
+            className="bg-white border border-gray-500 rounded-xl px-5 py-3"
+          >
+            {skill}
+          </li>
         ))}
       </ul>
     </section>
