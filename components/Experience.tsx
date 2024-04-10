77c21b803994aa84b55a7d1d587a050c9dd5625b
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-04-10 17:51:21 +0200

    logic - impliment react-timeline-component in Experience component

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -2,12 +2,29 @@
 import React from "react";
 import SectionHeading from "./SectionHeading";
 import { useSectionInView } from "@/lib/hooks";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import { experiencesData } from "@/lib/data";
 
 export default function Experience() {
   const { ref } = useSectionInView("Experience");
   return (
     <section id="experience" ref={ref}>
       <SectionHeading>My experience</SectionHeading>
+      <VerticalTimeline>
+        {experiencesData.map((item, index) => (
+          <React.Fragment key={index}>
+            <VerticalTimelineElement visible={true}>
+              <h3>{item.title}</h3>
+              <p>{item.location}</p>
+              <p>{item.description}</p>
+            </VerticalTimelineElement>
+          </React.Fragment>
+        ))}
+      </VerticalTimeline>
     </section>
   );
 }
