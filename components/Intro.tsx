28f087025617bcf89db01d7863beb7065927eefd
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-04-12 12:21:24 +0200

    fix - use setTimeOfLastClick when clicking Contact me button to make smooth navigation transition on the background highlight

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -7,9 +7,11 @@ import { BsArrowRight, BsLinkedin } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
 import { useSectionInView } from "@/lib/hooks";
+import { useActiveSectionContext } from "@/context/ActiveSectionContextProvider";
 
 export default function Intro() {
   const { ref } = useSectionInView("Home", 0.5);
+  const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
   return (
     <section
       id="home"
@@ -77,6 +79,10 @@ export default function Intro() {
           href="#contact"
           className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full 
           outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+          onClick={() => {
+            setActiveSection("Contact");
+            setTimeOfLastClick(Date.now());
+          }}
         >
           Contact me{" "}
           <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
