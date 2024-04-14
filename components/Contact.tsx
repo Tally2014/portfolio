53a0f5a665dd1cfa1c52eaa8d2d7bc47ddd9cb06
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-04-14 20:09:00 +0200

    logic - impliment next.js server action for for getting data from the form

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import SectionHeading from "./SectionHeading";
 import { motion } from "framer-motion";
 import SubmitBtn from "./SubmitBtn";
+import { sendEmail } from "@/actions/sendEmails";
 
 export default function Contact() {
   const { ref } = useSectionInView("Contact");
@@ -33,7 +34,16 @@ export default function Contact() {
         </a>{" "}
         or through this form.
       </p>
-      <form className="mt-10 flex flex-col">
+      <form
+        className="mt-10 flex flex-col"
+        action={async (formData) => {
+          console.log("Running on client");
+          console.log(formData.get("senderEmail"));
+          console.log(formData.get("message"));
+
+          await sendEmail(formData);
+        }}
+      >
         <input
           className="h-14 px-4 rounded-lg borderBlack"
           name="senderEmail"
