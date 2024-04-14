250645f09ac238edfa9542493098e91d78d34809
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-04-14 21:16:52 +0200

    refactor - create utils.ts validateString  helper function for email and message validation

--- actions/sendEmails.ts
+++ actions/sendEmails.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { Resend } from "resend";
+import { validateString } from "@/lib/utils";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
@@ -9,16 +10,23 @@ export const sendEmail = async (formData: FormData) => {
   const senderEmail = formData.get("senderEmail");
   const message = formData.get("message");
 
-  if (!message || typeof message !== "string") {
+  if (!validateString(senderEmail, 500)) {
+    return {
+      error: "Invalid sender email",
+    };
+  }
+
+  if (!validateString(message, 5000)) {
     return {
       error: "Invalid message",
     };
   }
 
-  resend.emails.send({
+  await resend.emails.send({
     from: "onboarding@resend.dev",
     to: "montsoernestmoalosi@gmail.com",
     subject: "Message from contact form",
+    reply_to: senderEmail,
     text: message,
   });
 };
