60d68e290d116d8415e4aeffda41bb56776c7ef2
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-04-15 21:59:38 +0200

    style - add dark mode classess in Contact.tsx and SubmitBtn component

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -28,7 +28,7 @@ export default function Contact() {
       }}
     >
       <SectionHeading>Contact</SectionHeading>
-      <p className="text-gray-700 -mt-6">
+      <p className="text-gray-700 -mt-6 dark:text-white/80">
         Please contact me directly at{" "}
         <a className="underline" href="mailto:montsoernesrmoalosi@gmail.com">
           montsoernesrmoalosi@gmail.com
@@ -36,7 +36,7 @@ export default function Contact() {
         or through this form.
       </p>
       <form
-        className="mt-10 flex flex-col"
+        className="mt-10 flex flex-col dark:text-black"
         action={async (formData) => {
           const { data, error } = await sendEmail(formData);
           if (error) {
@@ -47,14 +47,16 @@ export default function Contact() {
         }}
       >
         <input
-          className="h-14 px-4 rounded-lg borderBlack"
+          className="h-14 px-4 rounded-lg borderBlack
+          dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
           name="senderEmail"
           type="email"
           maxLength={500}
           placeholder="Your email"
         />
         <textarea
-          className="h-52 my-3 p-4 borderBlack rounded-lg"
+          className="h-52 my-3 p-4 borderBlack rounded-lg
+          dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
           name="message"
           placeholder="Your message"
           required
