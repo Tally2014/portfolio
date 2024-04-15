bedea98111a3d99a6a9fad5abd8fb03a9fd08b14
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-04-15 19:08:14 +0200

    style - useState Hook to toggle button theme and change the icon based on the selected theme (light or dark)

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -1,10 +1,25 @@
-import React from "react";
-import { BsSun } from "react-icons/bs";
+"use client";
+import React, { useState } from "react";
+import { BsSun, BsMoon } from "react-icons/bs";
+
+type Theme = "light" | "dark";
 
 export default function ThemeSwitch() {
+  const [theme, setTheme] = useState<Theme>("light");
+
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  };
   return (
-    <button className="w-[2rem] h-[2rem] border-2 borderBlack shadow-2xl rounded-full flex items-center justify-center text-gray-700 hover:scale-[1.15] active:scale-105 transition-all">
-      <BsSun />
+    <button
+      className="w-[2rem] h-[2rem] border-2 borderBlack shadow-2xl rounded-full flex items-center justify-center text-gray-700 hover:scale-[1.15] active:scale-105 transition-all"
+      onClick={toggleTheme}
+    >
+      {theme === "light" ? <BsSun /> : <BsMoon />}
     </button>
   );
 }
