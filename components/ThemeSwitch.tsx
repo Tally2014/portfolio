f074b1a90d678eaf36bf4c842b9e9b863841dc31
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-04-15 19:19:46 +0200

    logic - useEffect hook and localStorage to remeber the slected theme upon refresh

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BsSun, BsMoon } from "react-icons/bs";
 
 type Theme = "light" | "dark";
@@ -10,10 +10,19 @@ export default function ThemeSwitch() {
   const toggleTheme = () => {
     if (theme === "light") {
       setTheme("dark");
+      window.localStorage.setItem("theme", "dark");
     } else {
       setTheme("light");
+      window.localStorage.setItem("theme", "light");
     }
   };
+
+  useEffect(() => {
+    const localTheme = window.localStorage.getItem("theme") as Theme | null;
+    if (localTheme) {
+      setTheme(localTheme);
+    }
+  }, []);
   return (
     <button
       className="w-[2rem] h-[2rem] border-2 borderBlack shadow-2xl rounded-full flex items-center justify-center text-gray-700 hover:scale-[1.15] active:scale-105 transition-all"
