e429029719321cdcb2cdc30b88b00f3b16a1f209
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-04-15 21:42:52 +0200

    style - add dark mode classess in Intro.tsx

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -89,7 +89,7 @@ export default function Intro() {
         </Link>
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none 
-        focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack"
+        focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
           href="/CV.pdf"
           download
         >
@@ -98,14 +98,18 @@ export default function Intro() {
         </a>
         <div className="flex gap-2">
           <a
-            className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack"
+            className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full 
+            focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer 
+            borderBlack dark:bg-white/10 dark:text-white/60"
             href="https://www.linkedin.com/in/montso-moalosi-25334b246/"
             target="_blank"
           >
             <BsLinkedin />
           </a>
           <a
-            className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack"
+            className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full 
+            focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition 
+            cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
             href="https://github.com/Tally2014"
             target="_blank"
           >
