332330564c2b9d3c9407c9b935dd0e5166668518
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-04-18 06:08:59 +0200

    add - import my project realworld application and title projectsData

--- lib/data.ts
+++ lib/data.ts
@@ -2,9 +2,9 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "../public/corpcomment.png";
-import rmtdevImg from "../public/rmtdev.png";
-import wordanalyticsImg from "../public/wordanalytics.png";
+import project1 from "../public/project1.png";
+import project2 from "../public/project2.png";
+import project3 from "../public/project3.png";
 
 export const links = [
   {
@@ -62,25 +62,25 @@ export const experiencesData = [
 
 export const projectsData = [
   {
-    title: "CorpComment",
+    title: "Broadcasting Graphics",
     description:
       "I worked as a full-stack developer on this startup project for 2 years. Users can give feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    imageUrl: corpcommentImg,
+    tags: ["Maestro Designer", "Page editor", "News Controller"],
+    imageUrl: project1,
   },
   {
-    title: "rmtDev",
+    title: "Complex Live Graphics",
     description:
       "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
     tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
+    imageUrl: project2,
   },
   {
-    title: "Word Analytics",
+    title: "Full-Stack Development",
     description:
       "A web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
+    tags: ["C#", "WPF", "MVVM", "JSON", "XAML", "API"],
+    imageUrl: project3,
   },
 ] as const;
 
